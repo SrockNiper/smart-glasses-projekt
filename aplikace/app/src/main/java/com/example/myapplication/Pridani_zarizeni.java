3d3ad5dee724e6ce429de05ac344ec3586d06a94
SrockNiper <113103066+SrockNiper@users.noreply.github.com>
2022-11-26 12:05:43 +0100

    Nová aktivita
    Přidání nové aktivity

---
+++ aplikace/app/src/main/java/com/example/myapplication/Pridani_zarizeni.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pridani_zarizeni extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pridani_zarizeni);
+    }
+}
