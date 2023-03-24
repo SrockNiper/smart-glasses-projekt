6b62f221cacf10688642e3d1f09ca0020fc190ec
SrockNiper <113103066+SrockNiper@users.noreply.github.com>
2023-03-24 12:04:19 +0100

    reset aplikace

--- aplikace/app/src/main/java/com/example/myapplication/Pridani_zarizeni.java
+++ aplikace/app/src/main/java/com/example/myapplication/Pridani_zarizeni.java
@@ -19,155 +19,20 @@ import android.widget.Toast;
 
 public class Pridani_zarizeni extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int REQUEST_ENABLE_BT = 1;
 
-    private ListView listView;
-    private DeviceAdapter adapter;
-    private BluetoothAdapter bluetoothAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pridani_zarizeni);
-        int PERMISSION_ALL = 1;
-        String[] PERMISSIONS = {
-                Manifest.permission.BLUETOOTH,
-                Manifest.permission.BLUETOOTH_ADMIN,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.BLUETOOTH_SCAN
-        };
 
-        if(!hasPermissions(this, PERMISSIONS)){
-            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
-        }
-
-
-
-        // Find the ListView in the layout
-        listView = findViewById(R.id.list_view);
-
-        // Create an adapter to manage the data for the ListView
-        adapter = new DeviceAdapter(this, R.layout.list_item);
-
-        // Set the adapter on the ListView
-        listView.setAdapter(adapter);
-
-        // Get the Bluetooth adapter
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            // Bluetooth is not supported on this device
-            // Show an error message or disable the functionality
-            Toast.makeText(this, "Bluetooth is not supported on this device", Toast.LENGTH_SHORT).show();
-
-            return;
-        }
-
-        // Enable Bluetooth if it is not already enabled
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-
-        // Start discovering devices
-        discoverDevices();
     }
 
-    private static boolean hasPermissions(Context context, String... permissions) {
-        if (context != null && permissions != null) {
-            for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-
-    private void discoverDevices() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, REQUEST_SCAN_PERMISSION);
-        } else {
-            // Register a broadcast receiver to receive notifications when a device is discovered
-            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-            registerReceiver(mReceiver, filter);
-            // Start discovering devices
-            bluetoothAdapter.startDiscovery();
-        }
-    }
-
-
-
-    // Broadcast receiver for receiving notifications when a device is discovered
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                // Add the device to the adapter
-                adapter.addDevice(device);
-            }
-        }
-    };
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_ENABLE_BT) {
-            if (resultCode == RESULT_OK) {
-                // Bluetooth was enabled, start discovering devices
-                discoverDevices();
-            } else {
-                // User declined to enable Bluetooth, show an error message
-                Toast.makeText(this, "Error: Bluetooth must be enabled", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        // Make sure to unregister the receiver when the activity is destroyed
-        unregisterReceiver(mReceiver);
-        super.onDestroy();
-    }
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.connect_button) {
-            // A "Connect" button was clicked
-            // Get the device from the tag of the button
-            BluetoothDevice device = (BluetoothDevice) view.getTag();
-            // Initiate a connection to the device
-            // You will need to implement the necessary logic for connecting to a Bluetooth device
-        }
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_SCAN_PERMISSION) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                discoverDevices();
-            } else {
-                // handle the case where the user denied the permission
-                Toast.makeText(this, "The app cannot work without scan permission", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 
+    }
 }
