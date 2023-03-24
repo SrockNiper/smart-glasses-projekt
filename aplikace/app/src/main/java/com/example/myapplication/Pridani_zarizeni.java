1ed2fa9eec375c1955b813da7dd07f47dfab1f60
SrockNiper <113103066+SrockNiper@users.noreply.github.com>
2023-03-24 13:38:59 +0100

    test bl

--- aplikace/app/src/main/java/com/example/myapplication/Pridani_zarizeni.java
+++ aplikace/app/src/main/java/com/example/myapplication/Pridani_zarizeni.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,20 +15,54 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class Pridani_zarizeni extends AppCompatActivity implements View.OnClickListener {
-
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pridani_zarizeni);
-
+        ListView bllist = findViewById(R.id.list_view);
+        BluetoothManager bluetoothManager = getSystemService(BluetoothManager.class);
+        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+        ArrayList<String> items  = new ArrayList<>();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+        int REQUEST_ENABLE_BT = 0;
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+            bllist.setAdapter(adapter);
+            if (pairedDevices.size() > 0) {
+                // There are paired devices. Get the name and address of each paired device.
+                for (BluetoothDevice device : pairedDevices) {
+                    String deviceName = device.getName();
+                    String deviceHardwareAddress = device.getAddress(); // MAC address
+                        items.add(deviceName + "; ;" + deviceHardwareAddress);
+                        bllist.setAdapter(adapter);
+                }
+            }
+        }
     }
 
 
