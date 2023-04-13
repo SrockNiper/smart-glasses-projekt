cf5a7c096d065aac76dd7e9a03b60a572778c2a2
SrockNiper <113103066+SrockNiper@users.noreply.github.com>
2023-04-13 13:30:27 +0200

    Přidání čtení notifikací(ještě vyzkoušet)

--- aplikace/app/src/main/java/com/example/myapplication/Nastavovaci_stranka.java
+++ aplikace/app/src/main/java/com/example/myapplication/Nastavovaci_stranka.java
@@ -1,9 +1,20 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.Html;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Nastavovaci_stranka extends AppCompatActivity {
@@ -15,5 +26,29 @@ public class Nastavovaci_stranka extends AppCompatActivity {
         Intent intent = getIntent();
         String jmeno  = intent.getExtras().getString("jmeno");
         Toast.makeText(this,jmeno,Toast.LENGTH_LONG).show();
+        NotificationManager n = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(n.isNotificationPolicyAccessGranted()) {
+            }else{
+                startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
+            }
+        }
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
     }
+    private BroadcastReceiver onNotice= new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String packageName = intent.getStringExtra("package");
+            String titleData = intent.getStringExtra("title");
+            String textData = intent.getStringExtra("text");
+            TableRow tr = new TableRow(getApplicationContext());
+            tr.setLayoutParams(new TableRow.LayoutParams( TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+            TextView textview = new TextView(getApplicationContext());
+            textview.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT,1.0f));
+            textview.setTextSize(20);
+            textview.setTextColor(Color.parseColor("#0B0719"));
+            textview.setText(Html.fromHtml(packageName +"<br><b>" + titleData + " : </b>" + textData));
+            tr.addView(textview);
+        }
+    };
 }
