cf936b2e4beedfec837c0e99d64308610085fd3b
SrockNiper <113103066+SrockNiper@users.noreply.github.com>
2023-04-14 19:11:41 +0200

    Navázaná komunikace mezi aplikací a arduinem(nejspíš, je třeba další testování)

--- aplikace/app/src/main/java/com/example/myapplication/Nastavovaci_stranka.java
+++ aplikace/app/src/main/java/com/example/myapplication/Nastavovaci_stranka.java
@@ -1,11 +1,18 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.app.NotificationManager;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.Html;
@@ -14,23 +21,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import java.io.IOException;
+import java.util.UUID;
+
 public class Nastavovaci_stranka extends AppCompatActivity {
+    BluetoothAdapter myBluetooth = null;
+    BluetoothSocket btSocket = null;
+    private boolean isBtConnected = false;
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private ProgressDialog progress;
+    String adresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nastavovaci_stranka);
         Intent intent = getIntent();
-        String jmenod  = intent.getExtras().getString("jmeno");
-        Toast.makeText(this,jmenod,Toast.LENGTH_LONG).show();
-        if (jmenod != null){
+        String jmenod = intent.getExtras().getString("jmeno");
+        Toast.makeText(this, jmenod, Toast.LENGTH_LONG).show();
+        if (jmenod != null) {
             int delka = jmenod.length();
-            String adresa = jmenod.substring(delka-17);
-            Toast.makeText(this,adresa,Toast.LENGTH_LONG).show();
+            adresa = jmenod.substring(delka - 17);
+            Toast.makeText(this, adresa, Toast.LENGTH_LONG).show();
         }
-
+        new ConnectBT().execute();
         //NotificationManager n = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
 
@@ -39,6 +56,61 @@ public class Nastavovaci_stranka extends AppCompatActivity {
             startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
         }*/
         //LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean ConnectSuccess = true;
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(Nastavovaci_stranka.this, "Connecting...", "Please Wait!!!");
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+            try {
+                if (btSocket == null || !isBtConnected) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(adresa);
+                    if (ActivityCompat.checkSelfPermission(Nastavovaci_stranka.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+
+                    }
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();
+                }
+            } catch (IOException e) {
+                ConnectSuccess = false;
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute (Void result) {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess) {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            } else {
+                msg("Connected");
+                isBtConnected = true;
+            }
+
+            progress.dismiss();
+        }
     }
     private BroadcastReceiver onNotice= new BroadcastReceiver() {
         @Override
